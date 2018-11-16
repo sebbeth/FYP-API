3a244ce36522adcaa854b7541834178181e7baf0
Sebastian Brown <developer2025@gmail.com>
2018-11-16 21:09:36 +1100

    sdfghijkl

--- api.php
+++ api.php
@@ -16,7 +16,6 @@ require_once 'UploadInterface.php';
 require_once 'SolutionInterface.php';
 require_once 'ProviderInterface.php';
 require_once 'AccountInterface.php';
-require_once 'NormalisedSolution.php';
 
 
 header("Access-Control-Allow-Origin: $allowedOrigin");
